db77264a717087c41da94859679948d723645e7f
6076Mechanica <6076mechanica@gmail.com>
2024-03-23 09:25:22 -0700

    Working Swerve, dimensions are now in inches

--- src/main/java/frc/robot/subsystems/swervedrive/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/ClimberSubsystem.java
@@ -2,31 +2,37 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.swervedrive; 
+package frc.robot.subsystems.swervedrive;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.PneumaticsControlModule;
+import edu.wpi.first.wpilibj.PneumaticHub;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 
 public class ClimberSubsystem extends SubsystemBase {
 
-  private PneumaticsControlModule m_ControlModule = new PneumaticsControlModule();
-  private DoubleSolenoid m_doubleSolenoid = m_ControlModule.makeDoubleSolenoid(0, 1);
+  private DoubleSolenoid m_doubleSolenoid;
+  
   /** Creates a new ClimberSubsystem. */
   public ClimberSubsystem() {
-    //m_ControlModule.makeCompressor();
+    try (PneumaticsControlModule m_ControlModule = new PneumaticsControlModule()) {
+      m_doubleSolenoid = m_ControlModule.makeDoubleSolenoid(0, 1);
+      m_ControlModule.makeCompressor();
+    } 
+    m_doubleSolenoid.set(DoubleSolenoid.Value.kReverse);
   }
-
-  public void extend() {
-
-  }
-
-  public void retract() {
-  
-  } 
 
   public void toggle() {
     m_doubleSolenoid.toggle();
+  }
+
+  public void extend() {
+    m_doubleSolenoid.set(DoubleSolenoid.Value.kForward);
+  }
+
+  public void retract() {
+    m_doubleSolenoid.set(DoubleSolenoid.Value.kReverse);
   }
 
   @Override
